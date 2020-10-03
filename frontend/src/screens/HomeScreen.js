a559e44adbb8f800ac1eafbda76d7f430f70b404
AISHWARIYA SINGHA ROY <aishroy@amazon.com>
2020-10-03 22:52:45 +0530

    Fetch products from React (useEffect)

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React from 'react'
-import products from '../products'
+import React, { useState, useEffect } from 'react'
 import Product from '../components/Product'
 import { Container, Row, Col } from 'react-bootstrap'
+import axios from 'axios'
 
 const HomeScreen = () => {
+  /* what we call this piece of state
+  what is the name of the function which manipulates the state 
+  default for products */
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products')
+      setProducts(data)
+    }
+    fetchProducts()
+  }, [])
   return (
     <Container>
       <h1>LATEST PRODUCTS</h1>
