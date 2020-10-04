c8f781c35a5185de0eedbac7350e81170f1011b6
AISHWARIYA SINGHA ROY <aishroy@amazon.com>
2020-10-04 15:44:18 +0530

    Fetch products from db; start with Routers

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './data/products.js'
+import productRouter from './routes/productRouter.js'
 
 dotenv.config()
 
@@ -13,13 +13,9 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-  res.json(products)
-})
+app.use('/api/products', productRouter)
 
-app.get('/api/products/:id', (req, res) => {
-  res.json(products.find(p => p._id === req.params.id))
-})
+app.use('/api/products/:id', productRouter)
 
 app.listen(
   5000,
